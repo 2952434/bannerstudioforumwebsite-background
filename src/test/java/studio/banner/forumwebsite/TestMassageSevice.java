e59087475eecd0d80d81f120933e83eda8a6aa77
李君祥 <2952434583@qq.com>
2022-08-26 21:29:52 +0800

    接口修改

--- src/test/java/studio/banner/forumwebsite/TestMassageSevice.java
+++ src/test/java/studio/banner/forumwebsite/TestMassageSevice.java
@@ -1,153 +1,153 @@
-package studio.banner.forumwebsite;
-
-import com.alibaba.fastjson.JSON;
-import org.bson.types.ObjectId;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import springfox.documentation.spring.web.json.Json;
-import studio.banner.forumwebsite.bean.*;
-import studio.banner.forumwebsite.controller.frontdesk.PostController;
-import studio.banner.forumwebsite.mapper.FixedInformationMapper;
-import studio.banner.forumwebsite.mapper.MemberInformationMapper;
-import studio.banner.forumwebsite.mapper.PostMapper;
-import studio.banner.forumwebsite.mapper.UserContactMapper;
-import studio.banner.forumwebsite.service.*;
-import studio.banner.forumwebsite.service.impl.MessageServiceImpl;
-import studio.banner.forumwebsite.service.impl.UserAttentionServiceImpl;
-
-import java.util.*;
-
-@SpringBootTest
-public class TestMassageSevice {
-
-    @Autowired
-    private PostController postController;
-    @Autowired
-    private IMessageService messageService;
-    @Autowired
-    private UserContactMapper userContactMapper;
-    @Autowired
-    private FixedInformationMapper fixedInformationMapper;
-    @Autowired
-    private ICollectService iCollectService;
-    @Autowired
-    protected ICommentService iCommentService;
-
-    @Autowired
-    private PostMapper postMapper;
-    @Autowired
-    private IUserAttentionService iUserAttentionService;
-
-    @Autowired
-    private MemberInformationMapper memberInformationMapper;
-    @Autowired
-    private IPostEsService postEsService;
-    @Autowired
-    private ICommentService commentService;
-    @Test
-    public void test01(){
-//        List<FixedInformationBean> fixedInformationBeans = fixedInformationMapper.selectList(null);
-//        System.out.println(fixedInformationBeans);
-//        List<HashMap<String,String>> map = fixedInformationMapper.selectDirectionNum();
-//        System.out.println(JSON.toJSONString(map));
-//        List<HashMap<String, String>> hashMaps = fixedInformationMapper.selectDirectionPostNum();
-//        System.out.println(hashMaps.get(0).get("name"));
-//        List<String> list = fixedInformationMapper.selectUserGrade();
-//        System.out.println(list);
-//        List<Map<String, String>> maps = postMapper.selectListPost();
-//        System.out.println(maps);
+//package studio.banner.forumwebsite;
 //
-//        RespBean allWithPage = postEsService.findAllWithPage(1);
-//        System.out.println(allWithPage.getObj());
-
-        RespBean type = postEsService.findByPostContentAndPostTitleAndPostType("打扫", 99);
-        System.out.println(type.getObj());
-
-//PostBean postBean = new PostBean();
-//            postBean.setPostCommentNumber(1);
-//            postBean.setPostTime("123");
-//            postBean.setPostColNum(1);
-//            postBean.setPostPageView(12);
-//            postBean.setPostContent("dasfadad");
-//            postBean.setPostImageAddress("http");
-//            postBean.setPostTitle("你好");
-//            postBean.setPostMemberId(55);
-//            postBean.setPostType("Java,Python");
-//            postBean.setPostTop(0);
-//        for (int i = 0; i < 200; i++) {
+//import com.alibaba.fastjson.JSON;
+//import org.bson.types.ObjectId;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import springfox.documentation.spring.web.json.Json;
+//import studio.banner.forumwebsite.bean.*;
+//import studio.banner.forumwebsite.controller.frontdesk.PostController;
+//import studio.banner.forumwebsite.mapper.FixedInformationMapper;
+//import studio.banner.forumwebsite.mapper.MemberInformationMapper;
+//import studio.banner.forumwebsite.mapper.PostMapper;
+//import studio.banner.forumwebsite.mapper.UserContactMapper;
+//import studio.banner.forumwebsite.service.*;
+//import studio.banner.forumwebsite.service.impl.MessageServiceImpl;
+//import studio.banner.forumwebsite.service.impl.UserAttentionServiceImpl;
 //
-//            postMapper.insert(postBean);
+//import java.util.*;
+//
+//@SpringBootTest
+//public class TestMassageSevice {
+//
+//    @Autowired
+//    private PostController postController;
+//    @Autowired
+//    private IMessageService messageService;
+//    @Autowired
+//    private UserContactMapper userContactMapper;
+//    @Autowired
+//    private FixedInformationMapper fixedInformationMapper;
+//    @Autowired
+//    private ICollectService iCollectService;
+//    @Autowired
+//    protected ICommentService iCommentService;
+//
+//    @Autowired
+//    private PostMapper postMapper;
+//    @Autowired
+//    private IUserAttentionService iUserAttentionService;
+//
+//    @Autowired
+//    private MemberInformationMapper memberInformationMapper;
+//    @Autowired
+//    private IPostEsService postEsService;
+//    @Autowired
+//    private ICommentService commentService;
+//    @Test
+//    public void test01(){
+////        List<FixedInformationBean> fixedInformationBeans = fixedInformationMapper.selectList(null);
+////        System.out.println(fixedInformationBeans);
+////        List<HashMap<String,String>> map = fixedInformationMapper.selectDirectionNum();
+////        System.out.println(JSON.toJSONString(map));
+////        List<HashMap<String, String>> hashMaps = fixedInformationMapper.selectDirectionPostNum();
+////        System.out.println(hashMaps.get(0).get("name"));
+////        List<String> list = fixedInformationMapper.selectUserGrade();
+////        System.out.println(list);
+////        List<Map<String, String>> maps = postMapper.selectListPost();
+////        System.out.println(maps);
+////
+////        RespBean allWithPage = postEsService.findAllWithPage(1);
+////        System.out.println(allWithPage.getObj());
+//
+//        RespBean type = postEsService.findByPostContentAndPostTitleAndPostType("打扫", 99);
+//        System.out.println(type.getObj());
+//
+////PostBean postBean = new PostBean();
+////            postBean.setPostCommentNumber(1);
+////            postBean.setPostTime("123");
+////            postBean.setPostColNum(1);
+////            postBean.setPostPageView(12);
+////            postBean.setPostContent("dasfadad");
+////            postBean.setPostImageAddress("http");
+////            postBean.setPostTitle("你好");
+////            postBean.setPostMemberId(55);
+////            postBean.setPostType("Java,Python");
+////            postBean.setPostTop(0);
+////        for (int i = 0; i < 200; i++) {
+////
+////            postMapper.insert(postBean);
+////        }
+//
+////        List<Map<String, String>> maps = postMapper.selectPostById(0);
+////        System.out.println(maps);
+////        PostBean postBean = new PostBean(maps.get(0));
+////        System.out.println(postBean);
+//
+//    }
+//
+//    @Test
+//    public void testAttention(){
+//        UserAttentionBean userAttentionBean = new UserAttentionBean();
+//        userAttentionBean.setAttentionId(55);
+//        userAttentionBean.setBeAttentionId(197);
+//        userAttentionBean.setAttentionInformation("123");
+//        System.out.println(iUserAttentionService.insertContact(userAttentionBean));
+//        iUserAttentionService.deleteContact(55,197);
+//    }
+//
+//    @Test
+//    public void testCollect(){
+//        System.out.println(iCollectService.selectCollectFavoriteId(50, 1));
+//        System.out.println(iCollectService.judgeCollectPost(53, 1));
+//    }
+//
+//    @Test
+//    public void userContactMapper(){
+//        System.out.println(userContactMapper.selectFanByUserId(50, 0));
+//    }
+//
+//    @Test
+//    public void selectAllCommentByPostId(){
+//        System.out.println(commentService.selectAllCommentByPostId(1, 1).getObj());
+//    }
+//
+//    @Test
+//    public void testComment(){
+//        System.out.println(iCommentService.selectComment(130) != null);
+//    }
+//    @Test
+//    public void testSave() {
+//        Message message = Message.builder()
+//                .id(ObjectId.get())
+//                .msg("你好")
+//                .sendDate(new Date())
+//                .status(1)
+//                .from(new User(100, "zhangshan"))
+//                .to(new User(101, "lishi"))
+//                .build();
+//        this.messageService.saveMessage(message);
+//        message = Message.builder()
+//                .id(ObjectId.get())
+//                .msg("你也好")
+//                .sendDate(new Date())
+//                .status(1)
+//                .from(new User(101, "lishi"))
+//                .to(new User(100, "zhangshan"))
+//                .build();
+//        this.messageService.saveMessage(message);
+//    }
+//
+//    @Test
+//    public void testQueryList(){
+//        List<Message> listByFromAndTo = this.messageService.findListByFromAndTo(26, 25, 1, 10);
+//        for (Message message : listByFromAndTo) {
+//            System.out.println(message);
 //        }
-
-//        List<Map<String, String>> maps = postMapper.selectPostById(0);
-//        System.out.println(maps);
-//        PostBean postBean = new PostBean(maps.get(0));
-//        System.out.println(postBean);
-
-    }
-
-    @Test
-    public void testAttention(){
-        UserAttentionBean userAttentionBean = new UserAttentionBean();
-        userAttentionBean.setAttentionId(55);
-        userAttentionBean.setBeAttentionId(197);
-        userAttentionBean.setAttentionInformation("123");
-        System.out.println(iUserAttentionService.insertContact(userAttentionBean));
-        iUserAttentionService.deleteContact(55,197);
-    }
-
-    @Test
-    public void testCollect(){
-        System.out.println(iCollectService.selectCollectFavoriteId(50, 1));
-        System.out.println(iCollectService.judgeCollectPost(53, 1));
-    }
-
-    @Test
-    public void userContactMapper(){
-        System.out.println(userContactMapper.selectFanByUserId(50, 0));
-    }
-
-    @Test
-    public void selectAllCommentByPostId(){
-        System.out.println(commentService.selectAllCommentByPostId(1, 1).getObj());
-    }
-
-    @Test
-    public void testComment(){
-        System.out.println(iCommentService.selectComment(130) != null);
-    }
-    @Test
-    public void testSave() {
-        Message message = Message.builder()
-                .id(ObjectId.get())
-                .msg("你好")
-                .sendDate(new Date())
-                .status(1)
-                .from(new User(100, "zhangshan"))
-                .to(new User(101, "lishi"))
-                .build();
-        this.messageService.saveMessage(message);
-        message = Message.builder()
-                .id(ObjectId.get())
-                .msg("你也好")
-                .sendDate(new Date())
-                .status(1)
-                .from(new User(101, "lishi"))
-                .to(new User(100, "zhangshan"))
-                .build();
-        this.messageService.saveMessage(message);
-    }
-
-    @Test
-    public void testQueryList(){
-        List<Message> listByFromAndTo = this.messageService.findListByFromAndTo(26, 25, 1, 10);
-        for (Message message : listByFromAndTo) {
-            System.out.println(message);
-        }
-    }
-    @Test
-    public void test(){
-        postController.selectPostRank();
-    }
-}
+//    }
+//    @Test
+//    public void test(){
+//        postController.selectPostRank();
+//    }
+//}
