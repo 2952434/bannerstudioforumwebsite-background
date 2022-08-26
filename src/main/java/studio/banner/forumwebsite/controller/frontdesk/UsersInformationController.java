495502dffc1a46aa200a57027a2125d147e7f47a
李君祥 <2952434583@qq.com>
2022-08-26 20:03:36 +0800

    接口修改

--- src/main/java/studio/banner/forumwebsite/controller/frontdesk/UsersInformationController.java
+++ src/main/java/studio/banner/forumwebsite/controller/frontdesk/UsersInformationController.java
@@ -105,8 +105,8 @@ public class UsersInformationController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", header);
         HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
-//        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectGradeGroupBy/"+grade, HttpMethod.GET, httpEntity, RespBean.class);
-        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectGradeGroupBy/", HttpMethod.GET, httpEntity, RespBean.class);
+        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectGradeGroupBy/", HttpMethod.GET, httpEntity, RespBean.class);
+//        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectGradeGroupBy/", HttpMethod.GET, httpEntity, RespBean.class);
         return entity.getBody();
     }
 
@@ -117,8 +117,8 @@ public class UsersInformationController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", header);
         HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
-//        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectDirectionGroupBy", HttpMethod.GET, httpEntity, RespBean.class);
-        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectDirectionGroupBy"+grade, HttpMethod.GET, httpEntity, RespBean.class);
+        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectDirectionGroupBy"+grade, HttpMethod.GET, httpEntity, RespBean.class);
+//        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectDirectionGroupBy"+grade, HttpMethod.GET, httpEntity, RespBean.class);
         return entity.getBody();
     }
 
@@ -138,8 +138,8 @@ public class UsersInformationController {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", header);
         HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
-//        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectUserIdAndMemberName/{direction}/{grade}", HttpMethod.GET, httpEntity, RespBean.class,map);
-        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectUserIdAndMemberName/{direction}/{grade}", HttpMethod.GET, httpEntity, RespBean.class,map);
+        ResponseEntity<RespBean> entity = restTemplate.exchange("https://oauth.bannerstudio.club/admin/selectUserIdAndMemberName/{direction}/{grade}", HttpMethod.GET, httpEntity, RespBean.class,map);
+//        ResponseEntity<RespBean> entity = restTemplate.exchange("http://192.144.233.202:8090/admin/selectUserIdAndMemberName/{direction}/{grade}", HttpMethod.GET, httpEntity, RespBean.class,map);
         return entity.getBody();
     }
 
